a53c1254e62c0ecc0790cf57f339b57fac2a092e
Aryan Gore <rngore101@gmail.com>
2026-09-12 15:54:36 +0530

    Delete src directory

--- src/hosted/args.ts
+++
@@ -1,8 +0,0 @@
-import { parseCommandSurface, type ParsedCommandSurface } from '../command-surface.js';
-import type { HostedCommand } from './types.js';
-
-export type ParsedHostedInvocation = ParsedCommandSurface;
-
-export function parseHostedInvocation(command: HostedCommand, argv: string[]): ParsedHostedInvocation {
-  return parseCommandSurface(command, argv);
-}
